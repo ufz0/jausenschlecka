1bc8ebe51abaf80747c2cf92f886cc1a97a4a6ea
lukas <l.leutgeb@students.htl-leonding.ac.at>
2025-04-11 17:30:18 +0200

    Updated loading animation + main website area

--- main.go
+++ main.go
@@ -14,9 +14,12 @@ func main(){
 		c.File("public/index.html")
 	})
 
+
 	r.GET("/favicon.ico", func(c *gin.Context){
 		c.String(200, "Comming soon!")
 	})
+
+
 	fmt.Println("Server listening on http://127.0.0.1:8091")
 	r.Run(":8091")
 }
